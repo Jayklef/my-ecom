39f2edb5eaa37b1153e318dda1d3d89173cad38a
Jayklef <arhawhojerry@gmail.com>
2026-03-06 14:19:22 +0000

    cleared stock

--- myecom/settings.py
+++ myecom/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "category",
     "accounts",
+    "store",
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,14 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'myecom/static',
 ]
+
+
+# media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
