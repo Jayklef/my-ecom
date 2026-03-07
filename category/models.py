3709de23faef7bfa8cebd99020745ca4ac63188e
Jayklef <arhawhojerry@gmail.com>
2026-03-07 15:04:36 +0000

    added all category and context_processors

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,9 +9,15 @@ class Category(models.Model):
     description= models.TextField(max_length=255, blank= True)
     cat_image = models.ImageField(upload_to='photos/categories', blank=True)
     
+    # plural name in DB
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
-    
+        
+        
+    # CATEGORY URL LINK
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
+            
     def __str__(self):
         return self.category_name
