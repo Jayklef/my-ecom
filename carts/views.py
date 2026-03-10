e03f24726dd0ff09d93866aae23b26253120d88f
Jayklef <arhawhojerry@gmail.com>
2026-03-10 22:41:21 +0000

    Implemented product size and color and product Disparity

--- carts/views.py
+++ carts/views.py
@@ -13,6 +13,9 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    
+    color = request.GET['color']
+    size = request.GET['size']
     # get the product
     product = Product.objects.get(id=product_id)
     
