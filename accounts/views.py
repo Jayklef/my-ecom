403d19f1a763d4a57ff8b1a787005b139482acf6
Jayklef <arhawhojerry@gmail.com>
2026-03-14 08:23:43 +0000

    Implemented user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,34 @@
 from django.shortcuts import render
 from . forms import RegistrationForm
-
-
-
+from .models import Account
 
 # Create your views here.
 
 def register(request):
-    form = RegistrationForm()
-    
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        # When using django forms, you need to use cleaned_data to get the values from the request
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+    # next is to create user which is from the Django user which is from MyAccountManager class
+            
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            # since the MyAccountManager class does not include phonenumber, we now add phonenumber after creating the user
+            user.phone_number = phone_number
+            
+            #then save the user
+            user.save()       
+    else:
+        form = RegistrationForm()
+        
     context = {
-        'form': form
-    }
+            'form': form
+        }
     return render(request, 'accounts/register.html', context)
 
 
