f2ae77fc28a6c749b39b9fcc4111ef61adc06716
Jayklef <arhawhojerry@gmail.com>
2026-03-15 07:03:37 +0000

    Implemented verification link

--- myecom/settings.py
+++ myecom/settings.py
@@ -135,6 +135,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+# SMTP Configuration
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_PORT          = 587
+EMAIL_HOST_USER     = 'jerryarhawho@gmail.com'
+EMAIL_HOST_PASSWORD = 'cxrb ezny jtsn sodh'
+EMAIL_USE_TLS       = True
+
 
 # media files
 
